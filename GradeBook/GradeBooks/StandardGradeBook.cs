ee2f858edc176573bfe03500b7312b6542601d72
Raphael Haslebacher <raphael.haslebacher@zuehlke.com>
2019-05-07 13:26:27 +0200

    created enum

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name) => Type = GradeBookType.Standard;
+    }
+}
